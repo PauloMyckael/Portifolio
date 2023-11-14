fce9fe7817f5c3e4b17667caf4ede9f83a82b008
myckael <myckaelpaulo@gmail.com>
2023-11-14 14:29:39 -0300

    att

---
+++ cv/downloadCV.js
@@ -0,0 +1,4 @@
+window.addEventListener("DOMContentLoaded", function() {
+    const pdfContainer = document.getElementById("pdf-container");
+    pdfContainer.innerHTML = '<iframe src="seu-arquivo.pdf"></iframe>';
+});
